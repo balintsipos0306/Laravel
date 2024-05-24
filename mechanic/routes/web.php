83de1b412272f4fa28b06b8792989cf9ee90f8ca
bence <abalibence@gmail.com>
2024-05-24 16:59:04 +0200

    Javítva pár dolog

--- mechanic/routes/web.php
+++ mechanic/routes/web.php
@@ -15,18 +15,9 @@ Route::get('/login', function () {
     
 });
 
-<<<<<<< HEAD
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::delete('/logout', [LoginController::class, 'destroy']);
 
 Route::get('/main', function () {
     return view('main');
 });
-
-
-/*Route::get('/login/newAccount', function () {
-=======
-Route::get('/addNewWork', function () {
->>>>>>> 83a0968750bc092a9f590d86cc331c0c0accb033
-    return view('regist');
-});*/
